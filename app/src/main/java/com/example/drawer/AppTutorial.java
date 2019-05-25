7a840d26682e48f83a7078aeb2588fecd5e5e13c
andregual <andregual@gmail.com>
2019-05-25 20:03:52 +0100

    intro

--- app/src/main/java/com/example/drawer/AppTutorial.java
+++ app/src/main/java/com/example/drawer/AppTutorial.java
@@ -8,8 +8,6 @@ import android.support.v4.app.Fragment;
 import android.view.View;
 import android.widget.Toast;
 
-import com.example.drawer.R;
-import com.github.paolorotolo.appintro.AppIntro;
 import com.github.paolorotolo.appintro.AppIntro2;
 import com.github.paolorotolo.appintro.AppIntroFragment;
 import com.github.paolorotolo.appintro.model.SliderPage;
@@ -26,32 +24,39 @@ public class AppTutorial extends AppIntro2 {
 
 
         SliderPage sliderPage1 = new SliderPage();
-        sliderPage1.setTitle("Welcome!");
-        sliderPage1.setDescription("This is a demo of the AppIntro library, with a custom background on each slide!");
-        sliderPage1.setImageDrawable(R.drawable.mountain);
-        sliderPage1.setBgColor(Color.TRANSPARENT);
+        sliderPage1.setTitle("Welcome to OrganizeIt!");
+        sliderPage1.setDescription("OrganizeIt provides you with a platform for you to keep track of anything you need!");
+        sliderPage1.setImageDrawable(R.drawable.main);
+        sliderPage1.setBgColor(getColor(R.color.colorPrimary));
         addSlide(AppIntroFragment.newInstance(sliderPage1));
 
         SliderPage sliderPage2 = new SliderPage();
-        sliderPage2.setTitle("Clean App Intros");
-        sliderPage2.setDescription("This library offers developers the ability to add clean app intros at the start of their apps.");
-        sliderPage2.setImageDrawable(R.drawable.mountain);
-        sliderPage2.setBgColor(Color.TRANSPARENT);
+        sliderPage2.setTitle("You Can Create Boards");
+        sliderPage2.setDescription("Boards work like a project. Think of it as the cover of a book.");
+        sliderPage2.setImageDrawable(R.drawable.createboards);
+        sliderPage2.setBgColor(getColor(R.color.colorPrimary));
         addSlide(AppIntroFragment.newInstance(sliderPage2));
 
         SliderPage sliderPage3 = new SliderPage();
-        sliderPage3.setTitle("Simple, yet Customizable");
-        sliderPage3.setDescription("The library offers a lot of customization, while keeping it simple for those that like simple.");
-        sliderPage3.setImageDrawable(R.drawable.mountain);
-        sliderPage3.setBgColor(Color.TRANSPARENT);
+        sliderPage3.setTitle("Organize Your Boards By Adding Cards!");
+        sliderPage3.setDescription("Inside each board you can add cards. You can add a list of tasks to a card so you can keep track of the things you want!");
+        sliderPage3.setImageDrawable(R.drawable.createcards);
+        sliderPage3.setBgColor(getColor(R.color.colorPrimary));
         addSlide(AppIntroFragment.newInstance(sliderPage3));
 
         SliderPage sliderPage4 = new SliderPage();
-        sliderPage4.setTitle("Explore");
-        sliderPage4.setDescription("Feel free to explore the rest of the library demo!");
-        sliderPage4.setImageDrawable(R.drawable.mountain);
-        sliderPage4.setBgColor(Color.TRANSPARENT);
+        sliderPage4.setTitle("Manage Your Team!");
+        sliderPage4.setDescription("With OrganizeIt you can add collaborators to your Boards if you are working on something together1");
+        sliderPage4.setImageDrawable(R.drawable.manageteam);
+        sliderPage4.setBgColor(getColor(R.color.colorPrimary));
         addSlide(AppIntroFragment.newInstance(sliderPage4));
+
+        SliderPage sliderPage5 = new SliderPage();
+        sliderPage5.setTitle("You are Ready !");
+        sliderPage5.setDescription("You are now ready to start using the application! Feel free to explore other awesome features!");
+        sliderPage5.setImageDrawable(R.drawable.readystartexploring);
+        sliderPage5.setBgColor(getColor(R.color.colorPrimary));
+        addSlide(AppIntroFragment.newInstance(sliderPage5));
 
         // Bind the background to the intro
         //setBackgroundResource(R.drawable.ic_launcher_background);
